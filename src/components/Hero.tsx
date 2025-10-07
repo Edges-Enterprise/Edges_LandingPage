b36b23d54f147dace80aa3c7962b7e772a6aae3c
Bianca Uche <79880772+Erudite885@users.noreply.github.com>
2025-10-08 00:45:00 +0100

    Update Hero.tsx download button to v1.1.4

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -69,7 +69,8 @@ const Hero: React.FC = () => {
           className="mb-12"
         >
           <Link
-    href="https://files.fm/f/m6f9xk9kj8"
+ //   href="https://files.fm/f/m6f9xk9kj8"
+            href="https://files.fm/f/z327txaqrt"
     className="inline-block bg-blue-800 text-white px-12 py-2 rounded-3xl text-lg font-semibold hover:bg-blue-700"
   >
     Download Now
