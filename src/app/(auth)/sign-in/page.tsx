d98c47a9a81f730a63b12609a43765ab8e96bc26
Erudite885 <erudite885@gmail.com>
2025-10-29 14:36:58 +0100

    auth pages for sign in and sign up

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+// app/sign-in/page.tsx
+import SignInForm from '@/components/sign-in-form'; // Client Component below
+
+export default function SignInPage() {
+  return (
+    <main className="min-h-screen bg-black flex flex-col items-center justify-center px-4 sm:px-6 lg:px-20 py-8">
+      <div className="w-full max-w-md flex flex-col items-center">
+        <SignInForm />
+      </div>
+    </main>
+  );
+}
