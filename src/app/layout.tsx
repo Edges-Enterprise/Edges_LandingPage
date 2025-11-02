f2545df633d33d6a0d995c577f4c2ddc30c8dede
Erudite885 <erudite885@gmail.com>
2025-11-02 11:49:34 +0100

    auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "./providers/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -92,7 +93,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <AuthProvider>
+
         {children}
+        </AuthProvider>
 
         {/* ✅ Place structured data inside body to avoid hydration mismatch */}
         <script
