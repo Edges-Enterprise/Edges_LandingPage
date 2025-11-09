852d0d4c5004b9513abe40cf9108a0dd180ecb02
Erudite885 <erudite885@gmail.com>
2025-11-09 09:22:21 +0100

    sign-up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,5 @@
-import SignUpForm from '@/components/sign-up-form';
+import { SignUpForm } from "@/components/sign-up-form";
 
 export default function SignUpPage() {
-  return (
-    <main className="min-h-screen bg-black flex flex-col items-center justify-center px-4 sm:px-6 lg:px-20 py-8">
-      <div className="w-full max-w-md">
-        <SignUpForm />
-      </div>
-    </main>
-  );
+  return <SignUpForm />;
 }
