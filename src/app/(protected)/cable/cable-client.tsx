8fd85b9967e7fdb03e26fb6cc8098d058e1b9649
Erudite885 <erudite885@gmail.com>
2025-11-17 12:43:22 +0100

    cable client

--- src/app/(protected)/cable/cable-client.tsx
+++ src/app/(protected)/cable/cable-client.tsx
@@ -1,6 +1,6 @@
-// components/cable-client.tsx
+// app/(protected)/cable/cable-client.tsx
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import {
