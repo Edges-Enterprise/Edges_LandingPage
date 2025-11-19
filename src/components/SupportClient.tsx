02cf0ae9742033c2c009d38b94a9345856b7d460
Erudite885 <erudite885@gmail.com>
2025-11-19 11:37:33 +0100

    unused removed

--- src/components/SupportClient.tsx
+++ src/components/SupportClient.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   IoChatbubbleOutline,
-  IoMailOutline,
   IoHelpCircleOutline,
 } from "react-icons/io5";
 
