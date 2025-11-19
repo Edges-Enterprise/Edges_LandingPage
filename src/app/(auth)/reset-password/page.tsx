be755eb2d57a14824e8ce3eba7d9cbf3e7eb287b
Erudite885 <erudite885@gmail.com>
2025-11-19 17:20:33 +0100

    suspence around reset password

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import { ResetPasswordClient } from '@/components/reset-password'
+import { Suspense } from "react";
+import { ResetPasswordClient } from "@/components/reset-password";
 
 const ResetPassword = () => {
-  return <ResetPasswordClient/>
-  
-}
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen bg-black flex items-center justify-center">
+          <div className="text-white">Loading...</div>
+        </div>
+      }
+    >
+      <ResetPasswordClient />
+    </Suspense>
+  );
+};
+export default ResetPassword;
 
-export default ResetPassword
+// import React from 'react'
+// import { ResetPasswordClient } from '@/components/reset-password'
+
+// const ResetPassword = () => {
+//   return <ResetPasswordClient/>
+
+// }
+
+// export default ResetPassword
