a4c72c54fe54605aeff4451cfa602e4978ec08e2
Erudite885 <erudite885@gmail.com>
2026-02-11 11:38:59 +0100

    comment

--- src/app/(admin)/send-mail/page.tsx
+++ src/app/(admin)/send-mail/page.tsx
@@ -16,7 +16,7 @@ export default async function SendMailRoute() {
     .single();
 
   if (!profile?.is_admin) {
-    redirect("/notification"); // or wherever non-admins go
+    redirect("/notification"); // or wherever non-admins go, yh
   }
 
   return <AdminEmailPage />;
