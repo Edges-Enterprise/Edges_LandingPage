2e1ba9de5dbf46f6c442fdcc126f9fbdbaa3609e
Erudite885 <erudite885@gmail.com>
2026-07-17 07:28:29 +0100

    feat: create index file for application actions to streamline exports

---
+++ src/actions/reseller/application/index.ts
@@ -0,0 +1,5 @@
+// src/actions/reseller/application/index.ts
+export { saveDraft } from "./saveApplicationDraft";
+export { submitApplication } from "./submitApplication";
+export { getApplicationDraft } from "./getApplicationDraft";
+export { getApplicationStatus } from "./getApplicationStatus";
